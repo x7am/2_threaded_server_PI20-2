f1d190d124798d8bd39c52d7afa1cc6d4bacb3d7
Цветков Семён <65652418+x7am@users.noreply.github.com>
2021-11-06 21:08:22 +0300

    Updated client and server. Port scanner implemented.

--- server.py
+++ server.py
@@ -1,20 +1,39 @@
 import socket
+from threading import Thread
 
 sock = socket.socket()
-sock.bind(('', 9090))
+sock.bind(('', 52429))
 sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
 
-msg = ''
+users = []
 
-while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+def func():
+	conn, addr = sock.accept()
+	print(addr)
+	users.append(addr)
 
-print(msg)
+	msg = ''
 
-conn.close()
+	while True:
+		data = conn.recv(1024)
+		if not data:
+			break
+		msg = data.decode()
+		for i in users:
+			conn.sendto(data, (i))
+
+		print(msg)
+
+	conn.close()
+
+def main():
+	t1 = Thread(target=func)
+	t2 = Thread(target=func)
+	t3 = Thread(target=func)
+	t1.start()
+	t2.start()
+	t3.start()
+
+
+if __name__ == "__main__":
+	main()
